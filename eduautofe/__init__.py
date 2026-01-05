e5e49828689396e8dbf2d63937ed4202d02e04fe
lundkvistbenjamin <lundkvistbenjamin@gmail.com>
2026-01-05 12:47:00 +0200

    Restructured into folders.

--- eduautofe/__init__.py
+++ eduautofe/__init__.py
@@ -4,7 +4,7 @@ EduAutoFE - Educational Automated Feature Engineering
 A simple automated feature engineering tool designed for learning and education.
 """
 
-from eduautofe import EduAutoFE
+from .eduautofe import EduAutoFE
 
 __version__ = "0.1.0"
 __all__ = ["EduAutoFE"]
